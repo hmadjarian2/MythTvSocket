56f38d796a0433da1d495d97eedc2dc35d45caed
hmadjarian2 <hmadjarian@rogers.com>
2010-09-05 10:01:12 -0400

    apply naming conventions

--- myth_tv_socket.rb
+++ myth_tv_socket.rb
@@ -1,68 +1,68 @@
 require 'socket'
 
-def buildCommandString(command)
+def build_command_string(command)
   return "%-8d%s" % [command.length, command]
 end
 
 server = '192.168.1.10'
 port = 6543
-initialVersion = 50
+initial_version = 50
 delimiter = '[]:[]'
 reject = 'REJECT'
 ok = 'OK'
 
-versioncommand = '21      MYTH_PROTO_VERSION %s'
-anncommand = '23      ANN Playback sycamore 0'
-freerecordercountcommand = '23      GET_FREE_RECORDER_COUNT'
+version_command = '21      MYTH_PROTO_VERSION %s'
+ann_command = '23      ANN Playback sycamore 0'
+free_recorder_count_command = '23      GET_FREE_RECORDER_COUNT'
 
-puts 'Initial Version %s' % initialVersion
+puts 'Initial Version %s' % initial_version
 
 connection = TCPSocket::new(server, port)
 
-connection.write(versioncommand % initialVersion)
-byteCount = Integer(connection.recv(8))
+connection.write(version_command % initial_version)
+byte_count = Integer(connection.recv(8))
 
-bytesReceived = 0
+bytes_received = 0
 bytes = ''
 
-while bytesReceived < byteCount
-  bytes += connection.recv(byteCount - bytesReceived)
-  bytesReceived = bytes.length
+while bytes_received < byte_count
+  bytes += connection.recv(byte_count - bytes_received)
+  bytes_received = bytes.length
 end
 
 puts bytes
 
 response = bytes.split(delimiter)
 
-actualVersion = response[1]
-puts 'Actual Protocol %s' % actualVersion
+actual_version = response[1]
+puts 'Actual Protocol %s' % actual_version
 
 if response[0] == reject
   connection.close
   connection = TCPSocket::new(server, port)
-  connection.write(versioncommand % actualVersion)
-  byteCount = Integer(connection.recv(8))
+  connection.write(version_command % actual_version)
+  byte_count = Integer(connection.recv(8))
 
-  bytesReceived = 0
+  bytes_received = 0
   bytes = ''
 
-  while bytesReceived < byteCount
-    bytes += connection.recv(byteCount - bytesReceived)
-    bytesReceived = bytes.length
+  while bytes_received < byte_count
+    bytes += connection.recv(byte_count - bytes_received)
+    bytes_received = bytes.length
   end
 end
 
 puts bytes
 
-connection.write(anncommand)
-byteCount = Integer(connection.recv(8))
+connection.write(ann_command)
+byte_count = Integer(connection.recv(8))
 
-bytesReceived = 0
+bytes_received = 0
 bytes = ''
 
-while bytesReceived < byteCount
-  bytes += connection.recv(byteCount - bytesReceived)
-  bytesReceived = bytes.length
+while bytes_received < byte_count
+  bytes += connection.recv(byte_count - bytes_received)
+  bytes_received = bytes.length
 end
 
 puts bytes
@@ -73,15 +73,15 @@ if response[0] == ok
   puts 'ANN Playback is ok'
 end
 
-connection.write(freerecordercountcommand)
-byteCount = Integer(connection.recv(8))
+connection.write(free_recorder_count_command)
+byte_count = Integer(connection.recv(8))
 
-bytesReceived = 0
+bytes_received = 0
 bytes = ''
 
-while bytesReceived < byteCount
-  bytes += connection.recv(byteCount - bytesReceived)
-  bytesReceived = bytes.length
+while bytes_received < byte_count
+  bytes += connection.recv(byte_count - bytes_received)
+  bytes_received = bytes.length
 end
 
 puts bytes
